25358f4faf82b00ece9da3d779bddea41731fa72
four <gean.lc@alibaba-inc.com>
2022-09-19 15:18:13 +0800

    update

---
+++ tests/test_bytes.rs
@@ -0,0 +1,10 @@
+use std::str::from_utf8;
+use bytes::Bytes;
+
+#[test]
+fn test_bytes() {
+    let bytes = Bytes::from("abc".to_string());
+    let ss = from_utf8(&bytes).unwrap();
+    println!("{}", ss);
+    return;
+}
